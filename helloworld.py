dbc52eb1f33599104216fbddd09f0b4aa7e18908
Naveen123ond <146516685+Naveen123ond@users.noreply.github.com>
2024-03-14 10:01:00 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,9 @@
+# Get input from the user
+num1 = 5
+num2 = 5
 
-    print("Hello World!")
+# Add the numbers
+sum = num1 + num2
+
+# Print the result
+print("The sum of", num1, "and", num2, "is:", sum)
